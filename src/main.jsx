7d159a9b9589cb05121188dc392eb8c94a5281a4
Ramim Ahmed <ahramu584@gmail.com>
2025-12-13 12:52:07 +0600

    Result published2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
 )
